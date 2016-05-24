19ca45583a1f626cb1ce6937678b0324e4919cb2
tomwey <tomwey@163.com>
2016-05-24 09:41:18 +0800

    first

---
+++ Example/Pods/Target Support Files/AWMarkupStripper/AWMarkupStripper-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double AWMarkupStripperVersionNumber;
+FOUNDATION_EXPORT const unsigned char AWMarkupStripperVersionString[];
+
---
+++ Example/Pods/Target Support Files/Pods-AWMarkupStripper_Tests/Pods-AWMarkupStripper_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_AWMarkupStripper_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_AWMarkupStripper_Tests
+@end
